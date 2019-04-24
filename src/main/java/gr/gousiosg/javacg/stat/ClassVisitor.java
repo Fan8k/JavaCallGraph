f3cf9626f02e5a23fc75f50493b63aed6fd08646
Fan8k <17839227248@163.com>
2019-04-24 16:17:24 +0800

    java callGraph 关系数据库

--- src/main/java/gr/gousiosg/javacg/stat/ClassVisitor.java
+++ src/main/java/gr/gousiosg/javacg/stat/ClassVisitor.java
@@ -28,6 +28,9 @@
 
 package gr.gousiosg.javacg.stat;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.apache.bcel.classfile.Constant;
 import org.apache.bcel.classfile.ConstantPool;
 import org.apache.bcel.classfile.EmptyVisitor;
@@ -36,8 +39,7 @@ import org.apache.bcel.classfile.Method;
 import org.apache.bcel.generic.ConstantPoolGen;
 import org.apache.bcel.generic.MethodGen;
 
-import java.util.ArrayList;
-import java.util.List;
+import cn.fan.service.CallGraphBuilder;
 
 /**
  * The simplest of class visitors, invokes the method visitor class for each
@@ -45,55 +47,56 @@ import java.util.List;
  */
 public class ClassVisitor extends EmptyVisitor {
 
-    private JavaClass clazz;
-    private ConstantPoolGen constants;
-    private String classReferenceFormat;
-    private final DynamicCallManager DCManager = new DynamicCallManager();
-    private List<String> methodCalls = new ArrayList<>();
+	private JavaClass clazz;
+	private ConstantPoolGen constants;
+	private String classReferenceFormat;
+	private final DynamicCallManager DCManager = new DynamicCallManager();
+	private List<String> methodCalls = new ArrayList<>();
 
-    public ClassVisitor(JavaClass jc) {
-        clazz = jc;
-        constants = new ConstantPoolGen(clazz.getConstantPool());
-        classReferenceFormat = "C:" + clazz.getClassName() + " %s";
-    }
+	public ClassVisitor(JavaClass jc) {
+		clazz = jc;
+		constants = new ConstantPoolGen(clazz.getConstantPool());
+		classReferenceFormat = "C:" + clazz.getClassName() + " %s";
+	}
 
-    public void visitJavaClass(JavaClass jc) {
-        jc.getConstantPool().accept(this);
-        Method[] methods = jc.getMethods();
-        for (int i = 0; i < methods.length; i++) {
-            Method method = methods[i];
-            DCManager.retrieveCalls(method, jc);
-            DCManager.linkCalls(method);
-            method.accept(this);
+	public void visitJavaClass(JavaClass jc) {
+		jc.getConstantPool().accept(this);
+		Method[] methods = jc.getMethods();
+		for (int i = 0; i < methods.length; i++) {
+			Method method = methods[i];
+			DCManager.retrieveCalls(method, jc);
+			DCManager.linkCalls(method);
+			method.accept(this);
+		}
+	}
 
-        }
-    }
+	public void visitConstantPool(ConstantPool constantPool) {
+		for (int i = 0; i < constantPool.getLength(); i++) {
+			Constant constant = constantPool.getConstant(i);
+			if (constant == null)
+				continue;
+			if (constant.getTag() == 7) {
+				String referencedClass = constantPool.constantToString(constant);
+				// 输出当前哪个class 调用哪个class
+				// System.out.println(String.format(classReferenceFormat, referencedClass));
+			}
+		}
+	}
 
-    public void visitConstantPool(ConstantPool constantPool) {
-        for (int i = 0; i < constantPool.getLength(); i++) {
-            Constant constant = constantPool.getConstant(i);
-            if (constant == null)
-                continue;
-            if (constant.getTag() == 7) {
-                String referencedClass = 
-                    constantPool.constantToString(constant);
-                System.out.println(String.format(classReferenceFormat, referencedClass));
-            }
-        }
-    }
+	public void visitMethod(Method method) {
+		MethodGen mg = new MethodGen(method, clazz.getClassName(), constants);
+		// MethodVisitor visitor = new MethodVisitor(mg, clazz);
+		// 改成自己定义的方法遍历器
+		CallGraphBuilder visitor = new CallGraphBuilder(mg, clazz);
+		methodCalls.addAll(visitor.start());
+	}
 
-    public void visitMethod(Method method) {
-        MethodGen mg = new MethodGen(method, clazz.getClassName(), constants);
-        MethodVisitor visitor = new MethodVisitor(mg, clazz);
-        methodCalls.addAll(visitor.start());
-    }
+	public ClassVisitor start() {
+		visitJavaClass(clazz);
+		return this;
+	}
 
-    public ClassVisitor start() {
-        visitJavaClass(clazz);
-        return this;
-    }
-
-    public List<String> methodCalls() {
-        return this.methodCalls;
-    }
+	public List<String> methodCalls() {
+		return this.methodCalls;
+	}
 }
--- src/main/java/gr/gousiosg/javacg/stat/MethodVisitor.java
+++ src/main/java/gr/gousiosg/javacg/stat/MethodVisitor.java
@@ -28,90 +28,164 @@
 
 package gr.gousiosg.javacg.stat;
 
-import org.apache.bcel.classfile.JavaClass;
-import org.apache.bcel.generic.*;
-
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+import org.apache.bcel.classfile.JavaClass;
+import org.apache.bcel.generic.ConstantPoolGen;
+import org.apache.bcel.generic.ConstantPushInstruction;
+import org.apache.bcel.generic.EmptyVisitor;
+import org.apache.bcel.generic.INVOKEDYNAMIC;
+import org.apache.bcel.generic.INVOKEINTERFACE;
+import org.apache.bcel.generic.INVOKESPECIAL;
+import org.apache.bcel.generic.INVOKESTATIC;
+import org.apache.bcel.generic.INVOKEVIRTUAL;
+import org.apache.bcel.generic.Instruction;
+import org.apache.bcel.generic.InstructionConst;
+import org.apache.bcel.generic.InstructionHandle;
+import org.apache.bcel.generic.InvokeInstruction;
+import org.apache.bcel.generic.LineNumberGen;
+import org.apache.bcel.generic.MethodGen;
+import org.apache.bcel.generic.ReturnInstruction;
+import org.apache.bcel.generic.Type;
+
+import cn.fan.model.CodeLineNumber;
+
 /**
- * The simplest of method visitors, prints any invoked method
- * signature for all method invocations.
+ * The simplest of method visitors, prints any invoked method signature for all
+ * method invocations.
  * 
  * Class copied with modifications from CJKM: http://www.spinellis.gr/sw/ckjm/
  */
 public class MethodVisitor extends EmptyVisitor {
 
-    JavaClass visitedClass;
-    private MethodGen mg;
-    private ConstantPoolGen cp;
-    private String format;
-    private List<String> methodCalls = new ArrayList<>();
+	JavaClass visitedClass;
+	private MethodGen mg;
+	private ConstantPoolGen cp;
+	private String format;
+	private List<String> methodCalls = new ArrayList<>();
 
-    public MethodVisitor(MethodGen m, JavaClass jc) {
-        visitedClass = jc;
-        mg = m;
-        cp = mg.getConstantPool();
-        format = "M:" + visitedClass.getClassName() + ":" + mg.getName() + "(" + argumentList(mg.getArgumentTypes()) + ")"
-            + " " + "(%s)%s:%s(%s)";
-    }
+	public MethodVisitor(MethodGen m, JavaClass jc) {
+		visitedClass = jc;
+		mg = m;
+		cp = mg.getConstantPool();
+		format = "M:" + visitedClass.getClassName() + ":" + mg.getName() + "(" + argumentList(mg.getArgumentTypes())
+				+ ")" + " " + "(%s)%s:%s(%s)";
+	}
 
-    private String argumentList(Type[] arguments) {
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < arguments.length; i++) {
-            if (i != 0) {
-                sb.append(",");
-            }
-            sb.append(arguments[i].toString());
-        }
-        return sb.toString();
-    }
+	private String argumentList(Type[] arguments) {
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < arguments.length; i++) {
+			if (i != 0) {
+				sb.append(",");
+			}
+			sb.append(arguments[i].toString());
+		}
+		return sb.toString();
+	}
 
-    public List<String> start() {
-        if (mg.isAbstract() || mg.isNative())
-            return Collections.emptyList();
+	public List<String> start() {
+		if (mg.isAbstract() || mg.isNative())
+			return Collections.emptyList();
 
-        for (InstructionHandle ih = mg.getInstructionList().getStart(); 
-                ih != null; ih = ih.getNext()) {
-            Instruction i = ih.getInstruction();
-            
-            if (!visitInstruction(i))
-                i.accept(this);
-        }
-        return methodCalls;
-    }
+		// 先根据字节码和原代码之间的偏移关系 建立偏移表
+		LineNumberGen[] lineNumbers = mg.getLineNumbers();
+		List<CodeLineNumber> codeLineRelation = new ArrayList<CodeLineNumber>();
+		for (int i = 0; i < lineNumbers.length - 1; i++) {
+			codeLineRelation.add(new CodeLineNumber(lineNumbers[i].getLineNumber().getStartPC(),
+					lineNumbers[i + 1].getLineNumber().getStartPC() - 1,
+					lineNumbers[i].getLineNumber().getLineNumber()));
+		}
+		codeLineRelation.add(new CodeLineNumber(lineNumbers[lineNumbers.length - 1].getLineNumber().getStartPC(),
+				Integer.MAX_VALUE, lineNumbers[lineNumbers.length - 1].getLineNumber().getLineNumber()));
+		// 建立偏移表之后 就需要进行 指令规划 看看是哪一行
+		int currentInstruction = 0;
+		int byteIndex = 0;
+		int currentCodeLineNumber = 0;
+		int currentInstructionLineNumber = 0;
+		for (InstructionHandle ih = mg.getInstructionList().getStart(); ih != null; ih = ih.getNext()) {
+			Instruction i = ih.getInstruction();
+			// 先判断当前currentInstruction 是第几个
+			// 在判断属于哪一行源码
+			currentInstruction = mg.getInstructionList().getInstructionPositions()[byteIndex++];
+			CodeLineNumber codeLineNumber = codeLineRelation.get(currentCodeLineNumber);
+			if (currentInstruction > codeLineNumber.getEnd_pc()) {
+				currentCodeLineNumber++;
+				currentInstructionLineNumber = codeLineRelation.get(currentCodeLineNumber).getLine_number();
+			} else {
+				currentInstructionLineNumber = codeLineNumber.getLine_number();
+			}
+			if (!visitInstruction(i)) {
+				if (i instanceof InvokeInstruction) {
+					System.out.println(mg.getClassName() + mg.getName() + ":" + i.toString() + " : "
+							+ currentInstructionLineNumber);
+				} else {
+					i.accept(this);
+				}
+			}
+		}
+		return methodCalls;
+	}
 
-    private boolean visitInstruction(Instruction i) {
-        short opcode = i.getOpcode();
-        return ((InstructionConst.getInstruction(opcode) != null)
-                && !(i instanceof ConstantPushInstruction) 
-                && !(i instanceof ReturnInstruction));
-    }
+	public void way() {
+		LineNumberGen[] lineNumbers = mg.getLineNumbers();
+		// 有多少行就有多少个行号
+		int totol_lines = lineNumbers.length;
+		int index = 0;
+		// 第一个永远都有
+		LineNumberGen currentNumberGen = mg.getLineNumbers()[0];
+//		if (index < totol_lines) {
+//			currentNumberGen = lineNumbers[index++];
+//			InstructionHandle instruction = currentNumberGen.getInstruction();
+//			// 当前指令和源码中指令相似
+//			if (i.toString().equals(instruction.getInstruction().toString())
+//					&& instruction.getInstruction() instanceof InvokeInstruction) {
+//				System.out.println(mg.getClassName() + mg.getName() + ":" + i.toString() + " : "
+//						+ currentNumberGen.getSourceLine());
+//			}
+//		} else {
+//			if (i instanceof InvokeInstruction) {
+//				System.out.println(mg.getClassName() + mg.getName() + ":" + i.toString() + " : "
+//						+ currentNumberGen.getSourceLine());
+//			}
+//		}
+	}
 
-    @Override
-    public void visitINVOKEVIRTUAL(INVOKEVIRTUAL i) {
-        methodCalls.add(String.format(format,"M",i.getReferenceType(cp),i.getMethodName(cp),argumentList(i.getArgumentTypes(cp))));
-    }
+	private boolean visitInstruction(Instruction i) {
+		short opcode = i.getOpcode();
+		return ((InstructionConst.getInstruction(opcode) != null) && !(i instanceof ConstantPushInstruction)
+				&& !(i instanceof ReturnInstruction));
+	}
 
-    @Override
-    public void visitINVOKEINTERFACE(INVOKEINTERFACE i) {
-        methodCalls.add(String.format(format,"I",i.getReferenceType(cp),i.getMethodName(cp),argumentList(i.getArgumentTypes(cp))));
-    }
+	@Override
+	public void visitINVOKEVIRTUAL(INVOKEVIRTUAL i) {
+		methodCalls.add(String.format(format, "M", i.getReferenceType(cp), i.getMethodName(cp),
+				argumentList(i.getArgumentTypes(cp))));
+	}
 
-    @Override
-    public void visitINVOKESPECIAL(INVOKESPECIAL i) {
-        methodCalls.add(String.format(format,"O",i.getReferenceType(cp),i.getMethodName(cp),argumentList(i.getArgumentTypes(cp))));
-    }
+	@Override
+	public void visitINVOKEINTERFACE(INVOKEINTERFACE i) {
+		methodCalls.add(String.format(format, "I", i.getReferenceType(cp), i.getMethodName(cp),
+				argumentList(i.getArgumentTypes(cp))));
+	}
 
-    @Override
-    public void visitINVOKESTATIC(INVOKESTATIC i) {
-        methodCalls.add(String.format(format,"S",i.getReferenceType(cp),i.getMethodName(cp),argumentList(i.getArgumentTypes(cp))));
-    }
+	@Override
+	public void visitINVOKESPECIAL(INVOKESPECIAL i) {
 
-    @Override
-    public void visitINVOKEDYNAMIC(INVOKEDYNAMIC i) {
-        methodCalls.add(String.format(format,"D",i.getType(cp),i.getMethodName(cp),
-                argumentList(i.getArgumentTypes(cp))));
-    }
+		methodCalls.add(String.format(format, "O", i.getReferenceType(cp), i.getMethodName(cp),
+				argumentList(i.getArgumentTypes(cp))));
+	}
+
+	@Override
+	public void visitINVOKESTATIC(INVOKESTATIC i) {
+		methodCalls.add(String.format(format, "S", i.getReferenceType(cp), i.getMethodName(cp),
+				argumentList(i.getArgumentTypes(cp))));
+	}
+
+	@Override
+	public void visitINVOKEDYNAMIC(INVOKEDYNAMIC i) {
+		methodCalls.add(
+				String.format(format, "D", i.getType(cp), i.getMethodName(cp), argumentList(i.getArgumentTypes(cp))));
+	}
 }
